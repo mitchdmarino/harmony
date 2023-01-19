0cc6f7da59192982a519e5481930ad0ca709be7d
mitchdmarino <mitchdmarino@gmail.com>
2023-01-19 09:12:43 -0800

    questions done

--- src/components/connect/questions/AnswerBubble.js
+++ src/components/connect/questions/AnswerBubble.js
@@ -1,10 +1,21 @@
+import './AnswerBubble.css'
 import Profile from "../../misc/Profile";
 
-export default function AnswerBubble ({answer}) {
+export default function AnswerBubble ({answer, orderReverse}) {
+    var style = {
+        justifyContent: ""
+    }
+    if (orderReverse === 1) {
+        style = {
+            flexDirection: "row-reverse"
+        }
+    }
     return (
-        <div className="answer-bubble">
-            <Profile user={answer.author} />
-            <p>{answer.text}</p>
+        <div className="answer-bubble" style={style}>
+            <div>
+                <Profile user={answer.author} size="small" />
+            </div>
+            <div className="answer-text"><p>{answer.text}</p></div>
         </div>
     )
 }
--- src/components/connect/questions/Question.js
+++ src/components/connect/questions/Question.js
@@ -1,10 +1,8 @@
 import './Question.css'
 import Profile from "../../misc/Profile"
 import AnswerBubble from './AnswerBubble'
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import { answerQuestion, getQuestions } from '../../../utils/rest_api'
-
-
 
 export default function Question ({question, user,setQuestions}) {
     const [expand, setExpand] = useState(false)
@@ -24,7 +22,6 @@ export default function Question ({question, user,setQuestions}) {
     const didUserAnswerQuestion = () => {
         if(user) {
             for (let i=0; i<question.answers.length; i++) {
-                console.log(user)
                 if (question.answers[i].author._id===user._id) {
                     return true
                 }
@@ -48,32 +45,39 @@ export default function Question ({question, user,setQuestions}) {
 
 
     return (
-        <div className="question-container" onClick={() => setExpand(true)}>
-            <p>{question.question}</p>
+        <div className="question-container">
+            <h2 className='josefin-400'>{question.question}</h2>
             {!expand &&user? (
-                <div className='answered-by'>
-                    {answeredBy}
-                    {didUserAnswerQuestion()? "":(<div><p>Answer Question --{">"}</p></div>)}
+                <div className='answered-by' onClick={() => setExpand(true)}>
+                    <div className='answer-profiles'>{answeredBy}</div>
+                    {didUserAnswerQuestion()? "":(<div className='answer-prompt caveat-400'><p>Answer</p><img src="/icons/icons_answer.png" alt="next arrow pointing right"/></div>)}
                 </div>
             ): (
                 <div className='answers'>
                     {didUserAnswerQuestion()? (
                         question.answers.map((answer,i) => {
                             return (
-                                <AnswerBubble answer={answer} key={i}/>
+                                <AnswerBubble answer={answer} key={i} orderReverse={i}/>
                             )
                         })
                     ): (
-                        <div>
+                        <div className='answer-form josefin-400'>
                             <form onSubmit={(e) => handleAnswerQuestion(e)}>
                                 <label htmlFor='answer'>Answer</label>
-                                <input type="textarea" value={answer} onChange={(e)=> setAnswer(e.target.value)}/>
+                                <textarea className='josefin-400' value={answer} onChange={(e)=> setAnswer(e.target.value)}/>
                                 <button type='submit'>Submit</button>
                             </form>
                         </div>
                         )}
                 </div>
             )}
+            <div className='expand-button'>
+                {expand ? (
+                    <button onClick={() => setExpand(false)}><img src="/icons/icons_up.png" alt={"up arrow"}/></button>
+                ) : (
+                    <button onClick={() => setExpand(true)}><img src="/icons/icons_down.png" alt={"down arrow"}/></button>
+                )}
+            </div>
         </div>
     )
 }
