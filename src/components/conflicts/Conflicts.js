3721c1878a9ff7b57aed855817130bd2c12582a0
mitchdmarino <mitchdmarino@gmail.com>
2023-01-23 19:00:04 -0800

    sets conflicts as coming soon

--- src/components/conflicts/Conflicts.js
+++ src/components/conflicts/Conflicts.js
@@ -2,6 +2,6 @@ import './Conflicts.css'
 
 export default function Conflicts() {
     return (
-        <div className="conflicts">Conflicts</div>
+        <div className="conflicts" style={{padding: 10}}>Conflicts coming soon!</div>
     )
 }
