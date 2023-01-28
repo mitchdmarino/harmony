0b414d8d1e5dae525db8ca4cd0d70d897fbd835c
mitchdmarino <mitchdmarino@gmail.com>
2023-01-27 20:51:37 -0800

    Fixes photo squish

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,2 @@
-// export const REST_API_SERVER_URL = "http://localhost:8000/api-v1"
-export const REST_API_SERVER_URL = "https://smoggy-tam-newt.cyclic.app/api-v1"
+export const REST_API_SERVER_URL = "http://localhost:8000/api-v1"
+// export const REST_API_SERVER_URL = "https://smoggy-tam-newt.cyclic.app/api-v1"
